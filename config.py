8d5e185e2b11328e2043ee2db6b25d78e1642dcf
AlbertoRebolledo <alberto.rebolledo.j@gmail.com>
2023-01-13 12:53:09 +0000

    Archivo de configuración

---
+++ config.py
@@ -0,0 +1,3 @@
+from subprocess import call
+call(["git", "config", "--global", "user.email", "alberto.rebolledo.j@gmail.com"])
+call(["git", "config", "--global", "user.name", "\"AlbertoRebolledo\""])
