1833d12f26ba84cad2bb4c816a80c9b478f5ec51
Alan <alandiazdiazmingo@gmail.com>
2023-01-27 19:16:51 +0100

    reviews terminado

--- dockerCompose/compose.py
+++ dockerCompose/compose.py
@@ -6,17 +6,17 @@ def cambiarCompose(version):
     with open("docker-compose.yml", "r") as f:
         lines = f.readlines()
     if version == "v1":
-        lines_version = ['          - service_version=v1\n' if "service_version" in line else line for line in lines]
-        lines_ratings = ['          - enable_ratings=false\n' if "enable_ratings" in line else line for line in lines_version]
-        lines_color = ['          - star_color=black\n' if "star_color" in line else line for line in lines_ratings]
+        lines_version = ['      - SERVICE_VERSION=v1\n' if "SERVICE_VERSION" in line else line for line in lines]
+        lines_ratings = ['      - ENABLE_RATINGS=false\n' if "ENABLE_RATINGS" in line else line for line in lines_version]
+        lines_color = ['      - STAR_COLOR=black\n' if "STAR_COLOR" in line else line for line in lines_ratings]
     elif version == "v2":
-        lines_version = ['          - service_version=v2\n' if "service_version" in line else line for line in lines]
-        lines_ratings = ['          - enable_ratings=true\n' if "enable_ratings" in line else line for line in lines_version]
-        lines_color = ['          - star_color=black\n' if "star_color" in line else line for line in lines_ratings]
+        lines_version = ['      - SERVICE_VERSION=v2\n' if "SERVICE_VERSION" in line else line for line in lines]
+        lines_ratings = ['      - ENABLE_RATINGS=true\n' if "ENABLE_RATINGS" in line else line for line in lines_version]
+        lines_color = ['      - STAR_COLOR=black\n' if "STAR_COLOR" in line else line for line in lines_ratings]
     elif version == "v3":
-        lines_version = ['          - service_version=v3\n' if "service_version" in line else line for line in lines]
-        lines_ratings = ['          - enable_ratings=true\n' if "enable_ratings" in line else line for line in lines_version]
-        lines_color = ['          - star_color=red\n' if "star_color" in line else line for line in lines_ratings]
+        lines_version = ['      - SERVICE_VERSION=v3\n' if "SERVICE_VERSION" in line else line for line in lines]
+        lines_ratings = ['      - ENABLE_RATINGS=true\n' if "ENABLE_RATINGS" in line else line for line in lines_version]
+        lines_color = ['      - STAR_COLOR=red\n' if "STAR_COLOR" in line else line for line in lines_ratings]
     else:
         print("Elige una versión valida[v1, v2, v3]")
         exit()
@@ -42,10 +42,10 @@ call(["cp","practica_creativa2/bookinfo/src/ratings/package.json","Ratings/"])
 call(["cp","practica_creativa2/bookinfo/src/ratings/ratings.js","Ratings/"])
 
 #------------------------cosas de reviews----------------------------------------
-os.chdir(r'practica_creativa2/bookinfo/src/reviews/reviews-wlpcfg')
+os.chdir(r'practica_creativa2/bookinfo/src/reviews')
 pwd = os.getcwd()
 call(["sudo", "docker", "run", "--rm", "-u", "root", "-v", str(pwd)+":/home/gradle/project", "-w", "/home/gradle/project", "gradle:4.8.1", "gradle", "clean", "build"])
-os.chdir(r"../../../../../")
+os.chdir(r"../../../../")
 
 #--------------------------------creación de las imagenes del resto de microservicios-------------------------
 call(["sudo","docker","build","-t","g14/reviews","practica_creativa2/bookinfo/src/reviews/reviews-wlpcfg"])
